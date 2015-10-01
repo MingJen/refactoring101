5286292349b0326c7161ce69866d4557fc27d384
Teemo Wang <teemo@noc.tw>
2015-10-01 08:02:44 +0800

    Extract Method

--- code/001-begin.php
+++ code/001-begin.php
@@ -3,53 +3,33 @@
  * This is the beginning point of the refractoring journey. From here we have the
  * task of creating an HTML representation of the statement.  However, to get there
  * we have a bit of cleanup to do.
- * 
+ *
  */
 
 class Customer {
     public $name;
     public $rentals;
-    
+
     public function __construct($name) {
         $this->name = $name;
     }
-    
+
     public function addRental(Rental $rental) {
         $this->rentals[] = $rental;
     }
-    
+
     public function getName() {
         return $this->name;
     }
-    
+
     public function statement() {
         $totalAmount = 0;
         $frequentRenterPoints = 0;
         $result = "Rental Record for " . $this->getName() . "\n";
-        
+
         foreach ($this->rentals as $each) {
-            
-            $thisAmount = 0;
-            
-            switch ($each->movie->getPriceCode()) {
-                case Movie::REGULAR:
-            	    $thisAmount += 2;
-                    if ($each->getDaysRented() > 2) {
-                        $thisAmount += ($each->getDaysRented() - 2) * 1.5;
-                    }
-            	    break;
 
-                case Movie::NEW_RELEASE:
-                    $thisAmount += $each->getDaysRented() * 3;
-                    break;
-
-                case Movie::CHILDRENS:
-                    $thisAmount += 1.5;
-                    if ($each->getDaysRented() > 3) {
-                        $thisAmount += ($each->getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
+            $thisAmount = $this->amountFor($each);
 
             $frequentRenterPoints++;
 
@@ -65,8 +45,33 @@ class Customer {
         // add footer lines
         $result .= "Amount owed is " . $totalAmount . "\n";
         $result .= "You earned " . $frequentRenterPoints . " frequent renter points";
-        
+
         return $result;
+    }
+
+    private function amountFor($each)
+    {
+        $thisAmount = 0;
+        switch ($each->movie->getPriceCode()) {
+            case Movie::REGULAR:
+                $thisAmount += 2;
+                if ($each->getDaysRented() > 2) {
+                    $thisAmount += ($each->getDaysRented() - 2) * 1.5;
+                }
+                break;
+
+            case Movie::NEW_RELEASE:
+                $thisAmount += $each->getDaysRented() * 3;
+                break;
+
+            case Movie::CHILDRENS:
+                $thisAmount += 1.5;
+                if ($each->getDaysRented() > 3) {
+                    $thisAmount += ($each->getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return $thisAmount;
     }
 }
 
@@ -74,23 +79,23 @@ class Movie {
     const CHILDRENS = 2;
     const REGULAR = 0;
     const NEW_RELEASE = 1;
-    
+
     public $title;
     public $priceCode;
-    
+
     public function __construct($title, $priceCode) {
         $this->title = $title;
         $this->setPriceCode($priceCode);
     }
-    
+
     public function getPriceCode() {
         return $this->priceCode;
     }
-    
+
     public function setPriceCode($priceCode) {
         $this->priceCode = $priceCode;
     }
-    
+
     public function getTitle() {
         return $this->title;
     }
@@ -99,16 +104,16 @@ class Movie {
 class Rental {
     public $movie;
     public $daysRented;
-    
+
     public function __construct(Movie $movie, $daysRented) {
         $this->movie = $movie;
         $this->daysRented = $daysRented;
     }
-    
+
     public function getDaysRented() {
         return $this->daysRented;
     }
-    
+
     public function getMovie() {
         return $this->movie;
     }
