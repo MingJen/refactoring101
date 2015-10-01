735ad4c16cb8a55bf1f96d80294e7fbffd9fbe55
Teemo Wang <teemo@noc.tw>
2015-10-01 08:22:16 +0800

    Move Method

--- code/001-begin.php
+++ code/001-begin.php
@@ -29,7 +29,7 @@ class Customer {
 
         foreach ($this->rentals as $rental) {
 
-            $thisAmount = $this->getCharge($rental);
+            $thisAmount = $rental->getCharge();
 
             $frequentRenterPoints++;
 
@@ -46,31 +46,6 @@ class Customer {
         $result .= "Amount owed is " . $totalAmount . "\n";
         $result .= "You earned " . $frequentRenterPoints . " frequent renter points";
 
-        return $result;
-    }
-
-    private function getCharge($aRental)
-    {
-        $result = 0;
-        switch ($aRental->movie->getPriceCode()) {
-            case Movie::REGULAR:
-                $result += 2;
-                if ($aRental->getDaysRented() > 2) {
-                    $result += ($aRental->getDaysRented() - 2) * 1.5;
-                }
-                break;
-
-            case Movie::NEW_RELEASE:
-                $result += $aRental->getDaysRented() * 3;
-                break;
-
-            case Movie::CHILDRENS:
-                $result += 1.5;
-                if ($aRental->getDaysRented() > 3) {
-                    $result += ($aRental->getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
         return $result;
     }
 }
@@ -116,6 +91,31 @@ class Rental {
 
     public function getMovie() {
         return $this->movie;
+    }
+
+    public function getCharge()
+    {
+        $result = 0;
+        switch ($this->movie->getPriceCode()) {
+            case Movie::REGULAR:
+                $result += 2;
+                if ($this->getDaysRented() > 2) {
+                    $result += ($this->getDaysRented() - 2) * 1.5;
+                }
+                break;
+
+            case Movie::NEW_RELEASE:
+                $result += $this->getDaysRented() * 3;
+                break;
+
+            case Movie::CHILDRENS:
+                $result += 1.5;
+                if ($this->getDaysRented() > 3) {
+                    $result += ($this->getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return $result;
     }
 }
 
